9aa0e8c771627ea10c450ec418cd4a942b9ed4da
My Pham <muricapham@icloud.com>
2023-05-13 19:54:10 -0400

    Try vercel domain

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,9 @@ export default function Home({ pizzaList }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get(
+    "https://pizza-order-woad.vercel.app//api/products"
+  );
   return {
     props: {
       pizzaList: res.data,
